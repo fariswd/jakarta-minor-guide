5bdd3aac195d6af1dfdb9fea5136197a741894f6
Faris Widyantho <radenfaris@gmail.com>
2018-02-16 19:53:56 +0700

    add member CRD

--- server/controller/memberController.js
+++ server/controller/memberController.js
@@ -1,5 +1,73 @@
 const Member = require('../model/Member')
 
+const allMember = (req, res) => {
+  Member.find()
+  .then(response => {
+    res.send({
+      status: 'ok',
+      response: response
+    })
+  })
+  .catch(err => {
+    res.status(500).send({
+      status: 'err',
+      msg: 'cannot get member',
+      response: err
+    })
+  })
+}
+
+//require array
+const memberPost = (req, res) => {
+  Member.create(req.body)
+  .then(response => {
+    res.send({
+      status: 'ok',
+      response: response
+    })
+  })
+  .catch(err => {
+    res.status(500).send({
+      msg: 'cannot post a new member',
+      response: err
+    })
+  })
+}
+
+//require id
+const memberDetails = (req, res) => {
+  Member.findOne({ _id: req.params.id })
+  .then(response => {
+    res.send({
+      status: 'ok',
+      response: response
+    })
+  })
+  .catch(err => {
+    res.status(500).send({
+      status: 'err',
+      msg: 'cannot get member',
+      response: err
+    })
+  })
+}
+
+const memberDelete = (req, res) => {
+  Member.findByIdAndRemove(req.params.id)
+  .then(response => {
+    res.send({
+      status: 'ok',
+      response: response
+    })
+  })
+  .catch(err => {
+    res.status(500).send({
+      status: 'err',
+      msg: 'cannot get member',
+      response: err
+    })
+  })
+}
 
 
 module.exports = {
